7a22c5bded831c84eee0d3dafcf42bb47faa9b1d
Ivan Kazadaew <i.ziliboba@mail.ru>
2023-06-06 15:26:34 -0700

    Add wait

--- src/test/java/element/InputWithSearch.java
+++ src/test/java/element/InputWithSearch.java
@@ -2,6 +2,8 @@ package element;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class InputWithSearch extends AbstractElement {
     public final static String INPUT_WITH_SEARCH = "//*[contains(text(),'%s')]/parent::label/following-sibling::div/descendant::input";
@@ -11,6 +13,7 @@ public class InputWithSearch extends AbstractElement {
     }
 
     public void write(String text) {
+        new WebDriverWait(driver, WAIT_TIMEOUT_SECONDS).until(ExpectedConditions.presenceOfElementLocated(By.xpath(String.format(INPUT_WITH_SEARCH, label))));
         driver.findElement(By.xpath(String.format(INPUT_WITH_SEARCH, label))).sendKeys(text);
     }
 }
