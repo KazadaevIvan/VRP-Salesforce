783d2e3555f59799535e310cc81bd32b5377f243
Ivan Kazadaew <i.ziliboba@mail.ru>
2023-06-06 17:51:11 -0700

    Add test steps and precondition

--- src/test/java/test/AccountTest.java
+++ src/test/java/test/AccountTest.java
@@ -1,19 +1,42 @@
 package test;
 
+import model.Account;
+import model.User;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import page.AccountListPage;
 import page.LoginPage;
+import service.AccountCreator;
+import service.UserCreator;
+
+import static org.testng.Assert.assertEquals;
 
 public class AccountTest extends CommonConditions {
 
-    @Test(description = "Verify that user could create new account")
-    public void newAccountShouldBeCreated() {
+    @BeforeMethod
+    public void loginUser() {
+        User user = UserCreator.withCredentialsFromProperty();
+
         new LoginPage(driver)
                 .openPage()
-                .login("", "");
+                .login(user);
+    }
 
-        new AccountListPage(driver)
+    @Test(description = "Verify that user could create new account")
+    public void newAccountShouldBeCreated() {
+        Account account = AccountCreator.withParametersFromProperty();
+
+        Account actualAccount = new AccountListPage(driver)
                 .openPage()
-                .isPageOpened();
+                .isPageOpened()
+                .clickNewButton()
+                .isPageOpened()
+                .createAccount(account)
+                .clickSaveButton()
+                .isPageOpened()
+                .openAccountDetails()
+                .getAccountDetails();
+
+        assertEquals(actualAccount, account);
     }
 }
