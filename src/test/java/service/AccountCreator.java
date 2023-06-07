4d12463061b0610ad92653989efdde20955967ce
Ivan Kazadaew <i.ziliboba@mail.ru>
2023-06-06 22:54:42 -0700

    Add TestDataReader

--- src/test/java/service/AccountCreator.java
+++ src/test/java/service/AccountCreator.java
@@ -3,26 +3,44 @@ package service;
 import model.Account;
 
 public class AccountCreator {
+    public static final String ACCOUNT_NAME = TestDataReader.getTestData("accountName");
+    public static final String WEBSITE = TestDataReader.getTestData("website");
+    public static final String TYPE = TestDataReader.getTestData("type");
+    public static final String DESCRIPTION = TestDataReader.getTestData("description");
+    public static final String PHONE = TestDataReader.getTestData("phone");
+    public static final String INDUSTRY = TestDataReader.getTestData("industry");
+    public static final String EMPLOYEES = TestDataReader.getTestData("employees");
+    public static final String BILLING_STREET = TestDataReader.getTestData("billingStreet");
+    public static final String BILLING_CITY = TestDataReader.getTestData("billingCity");
+    public static final String BILLING_STATE = TestDataReader.getTestData("billingState");
+    public static final String BILLING_ZIP = TestDataReader.getTestData("billingZip");
+    public static final String BILLING_COUNTRY = TestDataReader.getTestData("billingCountry");
+    public static final String SHIPPING_STREET = TestDataReader.getTestData("shippingStreet");
+    public static final String SHIPPING_CITY = TestDataReader.getTestData("shippingCity");
+    public static final String SHIPPING_STATE = TestDataReader.getTestData("shippingState");
+    public static final String SHIPPING_ZIP = TestDataReader.getTestData("shippingZip");
+    public static final String SHIPPING_COUNTRY = TestDataReader.getTestData("shippingCountry");
+
 
     public static Account withParametersFromProperty() {
         return Account.builder()
-                .accountName("Account for testing")
-                .website("website")
-                .type("Analyst")
-                .description("description")
-                .phone("12345678")
-                .industry("Banking")
-                .employees("20")
-                .billingStreet("ul. Lenina")
-                .billingCity("Minsk")
-                .billingState("Minsk")
-                .billingZip("220140")
-                .billingCountry("Belarus")
-                .shippingStreet("ul. Pushkina")
-                .shippingCity("Minsk")
-                .shippingState("Minsk")
-                .shippingZip("220018")
-                .shippingCountry("Belarus")
+                .accountName(ACCOUNT_NAME)
+                .website(WEBSITE)
+                .type(TYPE)
+                .description(DESCRIPTION)
+                .phone(PHONE)
+                .industry(INDUSTRY)
+                .employees(EMPLOYEES)
+                .billingStreet(BILLING_STREET)
+                .billingCity(BILLING_CITY)
+                .billingState(BILLING_STATE)
+                .billingZip(BILLING_ZIP)
+                .billingCountry(BILLING_COUNTRY)
+                .shippingStreet(SHIPPING_STREET)
+                .shippingCity(SHIPPING_CITY)
+                .shippingState(SHIPPING_STATE)
+                .shippingZip(SHIPPING_ZIP)
+                .shippingCountry(SHIPPING_COUNTRY)
                 .build();
     }
 }
--- src/test/java/service/UserCreator.java
+++ src/test/java/service/UserCreator.java
@@ -3,7 +3,10 @@ package service;
 import model.User;
 
 public class UserCreator {
+    public static final String USERNAME = TestDataReader.getTestData("username");
+    public static final String PASSWORD = TestDataReader.getTestData("password");
+
     public static User withCredentialsFromProperty() {
-        return new User("", "");
+        return new User(USERNAME, PASSWORD);
     }
 }
